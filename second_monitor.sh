751e54f7ad494a43a33bde51bb85cadbace380a4
Paul Morris <paul@pmorris.org>
2020-06-11 23:08:28 +0100

    Lower second screen resolution

--- second_monitor.sh
+++ second_monitor.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-#RES="1920x1080"
+RES="1920x1080"
 #RES="2560x1440"
-RES="3840x2160"
+#RES="3840x2160"
 echo "Second monitor resolution: $RES"
 xrandr --output eDP-1 --auto --panning $RES --output DP-1 --mode $RES --same-as eDP-1
